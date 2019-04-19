58f2e347fb233e21767ea61772bbfc473100e554
V-i-C-o <vincent.costiou01@gmailcom>
2019-04-19 19:17:38 +0200

    Router with Semantic UI Menu

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Segment } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
+import './index.scss';
 
 const Navigation = () => (
-  <Menu>
-    <Menu.Item name="welcome" as={NavLink} to="/welcome">
-      Welcome
-    </Menu.Item>
-    <Menu.Item name="search" as={NavLink} to="/search">
-      Search
-    </Menu.Item>
-    <Menu.Item name="about" as={NavLink} to="/about">
-      A Propos
-    </Menu.Item>
-    <Menu.Item name="login" as={NavLink} to="/login">
-      Login
-    </Menu.Item>
-    <Menu.Item name="logout" as={NavLink} to="/logout">
-      Deconnexion
-    </Menu.Item>
-  </Menu>
+  <Segment inverted id="header">
+    <Menu inverted secondary id="nav">
+      <Menu.Item name="welcome" as={NavLink} to="/welcome">
+        Welcome
+      </Menu.Item>
+      <Menu.Item name="search" as={NavLink} to="/search">
+        Search
+      </Menu.Item>
+      <Menu.Item name="about" as={NavLink} to="/about">
+        A Propos
+      </Menu.Item>
+      <Menu.Item name="login" as={NavLink} to="/login">
+        Login
+      </Menu.Item>
+      <Menu.Item name="logout" as={NavLink} to="/logout">
+        Deconnexion
+      </Menu.Item>
+    </Menu>
+  </Segment>
 );
 
 export default Navigation;
